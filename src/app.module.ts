876c24628926076291994de9fec4da8bbe81fbf4
JoelRiveraL <joelale033@gmail.com>
2024-08-12 21:02:59 -0500

    Version 2 backend autenticacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthenticationModule } from './authentication/authentication.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,8 @@
 import { forwardRef,Module } from '@nestjs/common';
-import { UsersService } from './service/users.service';
-import { UsersController } from './controller/users.controller';
-import { AuthenticationModule } from '../authentication/authentication.module'; // Importa el AuthModule
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
-    imports: [forwardRef(() => AuthenticationModule)], // Utiliza forwardRef si hay dependencia circular
     controllers: [UsersController],
     providers: [UsersService],
     exports: [UsersService],
